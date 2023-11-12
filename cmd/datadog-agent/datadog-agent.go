c9c51fc14715eedd473b4f0d0662c0d67ceda286
cw-sakamoto <sakamoto@chatwork.com>
2023-11-13 08:34:40 +0900

    fix datadog-agent check test

--- cmd/datadog-agent/datadog-agent.go
+++ cmd/datadog-agent/datadog-agent.go
@@ -2,20 +2,17 @@ package datadogagent
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"time"
 
-	appsv1 "k8s.io/api/apps/v1"
-	apiv1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/cw-sakamoto/kibertas/cmd"
 	"github.com/cw-sakamoto/kibertas/config"
 	"github.com/cw-sakamoto/kibertas/util"
-	"github.com/cw-sakamoto/kibertas/util/k8s"
 	"github.com/cw-sakamoto/kibertas/util/notify"
 	"github.com/sirupsen/logrus"
 
@@ -25,10 +22,9 @@ import (
 
 type DatadogAgent struct {
 	*cmd.Checker
-	ApiKey         string
-	AppKey         string
-	ClusterName    string
-	DeploymentName string
+	ApiKey      string
+	AppKey      string
+	ClusterName string
 }
 
 func NewDatadogAgent(debug bool, logger func() *logrus.Entry, chatwork *notify.Chatwork) *DatadogAgent {
@@ -36,13 +32,12 @@ func NewDatadogAgent(debug bool, logger func() *logrus.Entry, chatwork *notify.C
 
 	namespace := fmt.Sprintf("datadog-agent-test-%d%02d%02d-%s", t.Year(), t.Month(), t.Day(), util.GenerateRandomString(5))
 
-	logger().Infof("datadog-agent check application namespace: %s\n", namespace)
+	logger().Infof("datadog-agent check application namespace: %s", namespace)
 	chatwork.AddMessage(fmt.Sprintf("datadog-agent check application namespace: %s\n", namespace))
 
 	apiKey := ""
 	appKey := ""
 	clusterName := ""
-	deploymentName := "sample-for-datadog"
 
 	if v := os.Getenv("DD_API_KEY"); v != "" {
 		apiKey = v
@@ -53,16 +48,11 @@ func NewDatadogAgent(debug bool, logger func() *logrus.Entry, chatwork *notify.C
 	if v := os.Getenv("CLUSTER_NAME"); v != "" {
 		clusterName = v
 	}
-	if v := os.Getenv("DEPLOYMENT_NAME"); v != "" {
-		deploymentName = v
-	}
-
 	return &DatadogAgent{
-		Checker:        cmd.NewChecker(namespace, config.NewK8sClientset(), debug, logger, chatwork),
-		ApiKey:         apiKey,
-		AppKey:         appKey,
-		ClusterName:    clusterName,
-		DeploymentName: deploymentName,
+		Checker:     cmd.NewChecker(namespace, config.NewK8sClientset(), debug, logger, chatwork),
+		ApiKey:      apiKey,
+		AppKey:      appKey,
+		ClusterName: clusterName,
 	}
 }
 
@@ -83,78 +73,41 @@ func (d *DatadogAgent) Check() error {
 	d.Chatwork.AddMessage("datadog-agent check start\n")
 	defer d.Chatwork.Send()
 
-	if err := d.prepareResources(); err != nil {
-		d.Chatwork.AddMessage(fmt.Sprintf("prepareResources error: %s\n", err.Error()))
-		return err
-	}
-	defer d.cleanupResources()
-
 	if err := d.checkMetrics(); err != nil {
 		d.Chatwork.AddMessage(fmt.Sprintf("checkMetrics error: %s\n", err.Error()))
 		return err
 	}
-	return nil
-}
-
-func (d *DatadogAgent) cleanupResources() error {
-	k := k8s.NewK8s(d.Namespace, d.Clientset, d.Debug, d.Logger)
-	k.DeleteDeployment(d.DeploymentName)
-	k.DeleteNamespace()
-	return nil
-}
-
-func (d *DatadogAgent) prepareResources() error {
-	k := k8s.NewK8s(d.Namespace, d.Clientset, d.Debug, d.Logger)
-	if err := k.CreateNamespace(&apiv1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: d.Namespace,
-		},
-	}); err != nil {
-		return err
-	}
-
-	if err := k.CreateDeployment(d.createDeploymentObject()); err != nil {
-		return err
-	}
+	d.Chatwork.AddMessage("datadog-agent check finished\n")
 	return nil
 }
 
 func (d *DatadogAgent) checkMetrics() error {
-	//d.Logger().Infof(d.ApiKey)
-	//d.Logger().Infof(d.AppKey)
+	keys := make(map[string]datadog.APIKey)
+	keys["apiKeyAuth"] = datadog.APIKey{Key: d.ApiKey}
+	keys["appKeyAuth"] = datadog.APIKey{Key: d.AppKey}
 
-	ctx := context.WithValue(
+	ddctx := datadog.NewDefaultContext(context.WithValue(
 		context.Background(),
 		datadog.ContextAPIKeys,
-		map[string]datadog.APIKey{
-			"apiKeyAuth": {
-				Key: d.ApiKey,
-			},
-			"appKeyAuth": {
-				Key: d.AppKey,
-			},
-		},
-	)
+		keys,
+	))
 
 	configuration := datadog.NewConfiguration()
 	apiClient := datadog.NewAPIClient(configuration)
-	metricsApi := datadogV1.NewMetricsApi(apiClient)
+	api := datadogV1.NewMetricsApi(apiClient)
+	query := fmt.Sprintf("avg:kubernetes.cpu.user.total{env:%s}", d.ClusterName)
 
+	d.Logger().Info("Waiting metrics...")
+	time.Sleep((60 * 3) * time.Second)
+
+	d.Logger().Infof("Querying metrics with query: %s", query)
 	now := time.Now().Unix()
-	from := now - 60*10
+	from := now - 60*2
+	err := wait.PollUntilContextTimeout(context.Background(), 30*time.Second, 10*time.Minute, true, func(ctx context.Context) (bool, error) {
+		resp, _, err := api.QueryMetrics(ddctx, from, now, query)
 
-	query := fmt.Sprintf(
-		"kubernetes.cpu.user.total{env:%s, kube_deployment:%s}", d.ClusterName, d.DeploymentName)
-
-	query = "system.cpu.idle{*}"
-
-	d.Logger().Infof("Querying metrics with query: %s\n", query)
-
-	resp, _, err := metricsApi.QueryMetrics(ctx, from, now, query)
-	err = wait.PollUntilContextTimeout(context.Background(), 10*time.Second, 10*time.Minute, false, func(ctx context.Context) (bool, error) {
-		resp, _, err = metricsApi.QueryMetrics(ctx, from, now, query)
 		if err != nil {
-			d.Logger().Errorf("Error when querying metrics: %v\n", err)
+			d.Logger().Errorf("Error when querying metrics: %v", err)
 			return false, err
 		}
 
@@ -162,7 +115,10 @@ func (d *DatadogAgent) checkMetrics() error {
 			d.Logger().Infof("No results found\n")
 			return false, nil
 		} else if len(resp.GetSeries()) > 0 {
-			d.Logger().Infof("Metrics Results found\n")
+			d.Logger().Infof("Response from `MetricsApi.QueryMetrics`")
+			d.Chatwork.AddMessage("Response from `MetricsApi.QueryMetrics`\n")
+			responseContent, _ := json.MarshalIndent(resp, "", "  ")
+			d.Logger().Debugf("Response:%s", responseContent)
 			return true, nil
 		}
 
@@ -173,44 +129,4 @@ func (d *DatadogAgent) checkMetrics() error {
 		return err
 	}
 	return nil
-}
-
-func (d *DatadogAgent) createDeploymentObject() *appsv1.Deployment {
-	deployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: d.DeploymentName,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: util.Int32Ptr(int32(1)),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": d.DeploymentName,
-				},
-			},
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": d.DeploymentName,
-					},
-				},
-				Spec: apiv1.PodSpec{
-					Containers: []apiv1.Container{
-						{
-							Name:  "nginx",
-							Image: "nginx:latest",
-							Ports: []apiv1.ContainerPort{
-								{
-									Name:          "http",
-									Protocol:      apiv1.ProtocolTCP,
-									ContainerPort: 80,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	return deployment
 }
