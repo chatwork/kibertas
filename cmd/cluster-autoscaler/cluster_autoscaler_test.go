c457750e457afc4ed9fba160187bceebe019b292
Tasuku Yamashita <task.teckac@gmail.com>
2025-08-25 10:06:31 +0900

    feat: Enhance cluster autoscaler tests with kwok provider integration and toleration support

--- cmd/cluster-autoscaler/cluster_autoscaler_test.go
+++ cmd/cluster-autoscaler/cluster_autoscaler_test.go
@@ -1,9 +1,10 @@
-//go:build ekstest
+//go:build !ekstest
 
 package clusterautoscaler
 
 import (
 	"context"
+	apiv1 "k8s.io/api/core/v1"
 	"os"
 	"testing"
 	"time"
@@ -23,8 +24,8 @@ func TestClusterAutoscalerScaleUpFromNonZero(t *testing.T) {
 	}
 
 	appName := "sample-for-scale"
-	capacityType := "SPOT"
-	// capacityType := "ON_DEMAND"
+	nodeLabelKey := "kwok-nodegroup"
+	nodeLabelValue := "kwok-worker"
 
 	h := testkit.New(t,
 		testkit.Providers(
@@ -38,18 +39,74 @@ func TestClusterAutoscalerScaleUpFromNonZero(t *testing.T) {
 
 	kc := h.KubernetesCluster(t)
 
+	kctl := testkit.NewKubectl(kc.KubeconfigPath)
+
 	k := testkit.NewKubernetes(kc.KubeconfigPath)
 	testkit.PollUntil(t, func() bool {
-		return len(k.ListReadyNodeNames(t)) > 1
+		// Only the control-plane node should be counted as Ready
+		return len(k.ListReadyNodeNames(t)) == 1
 	}, 20*time.Second)
 
 	helm := testkit.NewHelm(kc.KubeconfigPath)
 	// See https://github.com/kubernetes/autoscaler/tree/master/charts/cluster-autoscaler#tldr
+
+	helmInstallKwok(t, helm)
+	helmInstallClusterAutoscaler(t, helm, kctl)
+
+	os.Setenv("RESOURCE_NAME", appName)
+	os.Setenv("KUBECONFIG", kc.KubeconfigPath)
+	os.Setenv("NODE_LABEL_KEY", nodeLabelKey)
+	os.Setenv("NODE_LABEL_VALUE", nodeLabelValue)
+
+	logger := func() *logrus.Entry {
+		return logrus.NewEntry(logrus.New())
+	}
+	chatwork := &notify.Chatwork{
+		Logger: logger,
+	}
+	checker := cmd.NewChecker(context.Background(), false, logger, chatwork, "test", 7*time.Minute)
+	clusterautoscaler, err := NewClusterAutoscaler(checker)
+	if err != nil {
+		t.Fatalf("NewClusterAutoscaler: %s", err)
+	}
+
+	// Configure tolerations for kwok provider nodes
+	// This allows test pods to be scheduled on kwok-simulated nodes with taints
+	clusterautoscaler.SetDeploymentOption(DeploymentOption{
+		Tolerations: []apiv1.Toleration{
+			{
+				Key:      "kwok-provider",
+				Operator: apiv1.TolerationOpEqual,
+				Value:    "true",
+				Effect:   apiv1.TaintEffectNoSchedule,
+			},
+		},
+	})
+
+	if clusterautoscaler == nil {
+		t.Error("Expected clusterautoscaler instance, got nil")
+	}
+
+	require.NoError(t, wait.PollUntilContextTimeout(context.Background(), 5*time.Second, 5*time.Minute, false, func(ctx context.Context) (bool, error) {
+		nodes := k.ListReadyNodeNames(t)
+		return len(nodes) == 1, nil
+	}))
+
+	// Scale from 1 to 2
+	require.NoError(t, clusterautoscaler.Check())
+	require.NoError(t, wait.PollUntilContextTimeout(context.Background(), 5*time.Second, 5*time.Minute, false, func(ctx context.Context) (bool, error) {
+		nodes := k.ListReadyNodeNames(t)
+		return len(nodes) == 2, nil
+	}))
+}
+
+func helmInstallClusterAutoscaler(t *testing.T, helm *testkit.Helm, kctl *testkit.Kubectl) {
 	helm.AddRepo(t, "autoscaler", "https://kubernetes.github.io/autoscaler")
 
 	clusterautoscalerNs := "default"
 	helm.UpgradeOrInstall(t, "cluster-autoscaler", "autoscaler/cluster-autoscaler", func(hc *testkit.HelmConfig) {
 		hc.Values = map[string]interface{}{
+			"cloudProvider": "kwok",
 			"autoDiscovery": map[string]interface{}{
 				// This is so because we specify prefix=kibertas-ca in the testkit constructor above
 				// and the terraform main.tf uses prefix + "-cluster" as the cluster name.
@@ -76,46 +133,25 @@ func TestClusterAutoscalerScaleUpFromNonZero(t *testing.T) {
 		hc.Namespace = clusterautoscalerNs
 	})
 
-	os.Setenv("RESOURCE_NAME", appName)
-	os.Setenv("KUBECONFIG", kc.KubeconfigPath)
-	// os.Setenv("NODE_LABEL_VALUE", "ON_DEMAND")
-	os.Setenv("NODE_LABEL_VALUE", capacityType)
+	// Apply custom node templates for kwok provider to define node specifications
+	kctl.Capture(t,
+		"apply", "-n", clusterautoscalerNs, "-f", "testdata/kwok-provider-templates.yaml",
+	)
+	// Restart cluster-autoscaler to load the new node templates
+	kctl.Capture(t,
+		"rollout", "restart", "deployment", "cluster-autoscaler-kwok-cluster-autoscaler",
+		"-n", clusterautoscalerNs,
+	)
+}
 
-	logger := func() *logrus.Entry {
-		return logrus.NewEntry(logrus.New())
-	}
-	chatwork := &notify.Chatwork{
-		Logger: logger,
-	}
-	checker := cmd.NewChecker(context.Background(), false, logger, chatwork, "test", 7*time.Minute)
-	clusterautoscaler, err := NewClusterAutoscaler(checker)
-	if err != nil {
-		t.Fatalf("NewClusterAutoscaler: %s", err)
-	}
+func helmInstallKwok(t *testing.T, helm *testkit.Helm) {
+	helm.AddRepo(t, "kwok", "https://kwok.sigs.k8s.io/charts")
 
-	if clusterautoscaler == nil {
-		t.Error("Expected clusterautoscaler instance, got nil")
-	}
-
-	initialNodes := len(k.ListReadyNodeNames(t))
-
-	// Scale from 1 to 2
-	require.NoError(t, clusterautoscaler.Check())
-	require.NoError(t, wait.PollUntilContextTimeout(context.Background(), 5*time.Second, 5*time.Minute, false, func(ctx context.Context) (bool, error) {
-		nodes := k.ListReadyNodeNames(t)
-		return len(nodes) == initialNodes+1, nil
-	}))
-
-	// Scale to 0
-	require.NoError(t, wait.PollUntilContextTimeout(context.Background(), 5*time.Second, 10*time.Minute, false, func(ctx context.Context) (bool, error) {
-		nodes := k.ListReadyNodeNames(t)
-		return len(nodes) == 2, nil
-	}))
-
-	// Scale from 0 to 1
-	require.NoError(t, clusterautoscaler.Check())
-	require.NoError(t, wait.PollUntilContextTimeout(context.Background(), 5*time.Second, 8*time.Minute, false, func(ctx context.Context) (bool, error) {
-		nodes := k.ListReadyNodeNames(t)
-		return len(nodes) == 3, nil
-	}))
+	helm.UpgradeOrInstall(t, "kwok", "kwok/kwok", func(hc *testkit.HelmConfig) {
+		hc.Namespace = "kube-system"
+	})
+	// why: install stage rules to simulate Pod/Node lifecycle in a KWOK simulated cluster
+	helm.UpgradeOrInstall(t, "kwok-stage-fast", "kwok/stage-fast", func(hc *testkit.HelmConfig) {
+		hc.Namespace = "kube-system"
+	})
 }
