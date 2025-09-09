2db0225f1b4d52bbdd95cc81662cc6534aba0f1e
Tasuku Yamashita <task.teckac@gmail.com>
2025-09-09 14:58:58 +0900

    test: move helmInstallKwok function to helper_test.go

--- cmd/cluster-autoscaler/cluster_autoscaler_test.go
+++ cmd/cluster-autoscaler/cluster_autoscaler_test.go
@@ -140,16 +140,3 @@ func helmInstallClusterAutoscaler(t *testing.T, helm *testkit.Helm, kctl *testki
 		"-n", clusterautoscalerNs,
 	)
 }
-
-func helmInstallKwok(t *testing.T, helm *testkit.Helm) {
-	helm.AddRepo(t, "kwok", "https://kwok.sigs.k8s.io/charts")
-
-	helm.UpgradeOrInstall(t, "kwok", "kwok/kwok", func(hc *testkit.HelmConfig) {
-		hc.Namespace = "kube-system"
-	})
-	// why: install stage rules to simulate Pod/Node lifecycle in a KWOK simulated cluster
-	helm.UpgradeOrInstall(t, "kwok-stage-fast", "kwok/stage-fast", func(hc *testkit.HelmConfig) {
-		hc.Namespace = "kube-system"
-	})
-	t.Logf("KWOK installed successfully")
-}
